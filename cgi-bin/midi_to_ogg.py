3796a8cc56da1b6fda7598ccbc905da53334dcfc
Jesse Kaukonen <jesse.kaukonen@gmail.com>
2014-06-08 20:28:16 +0300

    script works, but has some bug with filling the audio object with a newly generated ogg. Added debug log

--- cgi-bin/midi_to_ogg.py
+++ cgi-bin/midi_to_ogg.py
@@ -12,6 +12,8 @@ midi_dir = "../midis/"
 convert_command = "/usr/bin/timidity"
 convert_flags = ["-Ow", "-o"]
 compress_command = "/usr/bin/oggenc"
+log_file = midi_dir + "log.txt"
+write_log = True
 
 def printHeader():
     print "Content-Type: text/html\n"
@@ -31,7 +33,7 @@ def convert(original_path, converted_path):
     wav_path = converted_path.replace(".ogg", ".wav")
 
     try:
-        p = subprocess.Popen([convert_command, original_path, convert_flags[0], convert_flags[1], wav_path], close_fds=True, shell=False)
+        p = subprocess.Popen([convert_command, original_path, convert_flags[0], convert_flags[1], wav_path])
         p.wait()
         p2 = subprocess.Popen([compress_command, wav_path], close_fds=True, stdin=None, stdout=None, stderr=None, shell=False)
         p2.wait()
@@ -40,63 +42,134 @@ def convert(original_path, converted_path):
         print(e)
 
 if __name__ == "__main__":
-    cgitb.enable()
+    f = None
 
-    path = None
-    form = cgi.FieldStorage()
+    if (write_log):
+        if (os.path.isfile(log_file) == True):
+            os.remove(log_file)
+        f = open(log_file, 'w')
+        f.write("Opening log file\n")
+
+    try:
+        cgitb.enable()
+    except Exception as e:
+        if (write_log):
+            f.write("Error at running cgitb.enable()\n")
+            f.write(e)
+            f.close()
+        sys.exit()
+
+    try:
+        path = None
+        form = cgi.FieldStorage()
+        if (write_log):
+            f.write("Opened fieldstorage\n")
+    except Exception as e:
+        if (write_log):
+            f.write("Exception opening fieldstorage:\n")
+            f.write(e)
+            f.close()
 
     try:
         path = form["path"].value
+        if (write_log):
+            f.write("Got path: " + path + "\n")
     except Exception as e:
-        print(e)
-        printHeader()
-        print "POST request did not have a path in it"
-        finish()
+        if (write_log):
+            f.write("Exception getting path: " + path + "\n")
+            f.write(e)
+            f.close()
+        sys.exit()
  
-    better_input = clean_input(path)[0]
-    fullpath = midi_dir + better_input
-
-    if (os.path.isfile(fullpath) == False):
-        printHeader()
-        print "Error: File does not exist: " + fullpath
-        finish()
+    try:
+        better_input = clean_input(path)[0]
+        fullpath = midi_dir + better_input
+        if (write_log):
+            f.write("Cleaned input path successfully\n")
+    except Exception as e:
+        if (write_log):
+            f.write("Error cleaning input: " + path + "\n")
+            f.write(e)
+            f.close()
         sys.exit()
 
-    converted_path = fullpath.replace(".mid", ".ogg")
+    try:
+        if (os.path.isfile(fullpath) == False):
+            if (write_log):
+                f.write("File does not exist: " + fullpath + "\n")
+                f.close()
+            sys.exit()
+    except Exception as e:
+        if (write_log):
+            f.write("Error checking if file exists: " + fullpath + "\n")
+            f.close()
+        sys.exit()
+
+    try:
+        converted_path = fullpath.replace(".mid", ".ogg")
+        if (write_log):
+            f.write("Converted path is " + converted_path + "\n")
+    except Exception as e:
+        if (write_log):
+            f.write("Exception at creating converted filepath:\n")
+            f.write(e)
+            f.close()
+        sys.exit()
  
-    # If the ogg is already converted
-    if (os.path.isfile(converted_path) == True):
-        try:
+    try:
+        # If the ogg is already converted
+        if (os.path.isfile(converted_path) == True):
+            try:
+                encoded = urllib.quote(open(converted_path, "rb").read().encode("base64"))
+                printHeader()
+                print(encoded)
+                finish()
+                if (write_log):
+                    f.write("Returned pre-encoded ogg!\n")
+                    f.close()
+                sys.exit()
+            except Exception as e:
+                if (write_log):
+                    f.write("Error returning pre-recorded ogg:\n")
+                    f.write(e)
+                    f.close()
+                sys.exit()
+    except Exception as e:
+        if (write_log):
+            f.write("Error calling os.path.isfile()\n")
+            f.close()
+        sys.exit()
+
+    # Time to convert!
+    try:
+        convert(fullpath, converted_path)
+        if (write_log):
+            f.write("Converted " + fullpath + " to " + converted_path + "\n")
+    except Exception as e:
+        if (write_log):
+            f.write("Error converting file:\n")
+            f.write(e)
+            f.close()
+        sys.exit()
+   
+    try:
+        if (os.path.isfile(converted_path) == True):
             encoded = urllib.quote(open(converted_path, "rb").read().encode("base64"))
             printHeader()
             print(encoded)
             finish()
-            sys.exit()
-        except Exception as e:
-            print(e)
-            printHeader()
-            print "Error returning pre-converted ogg"
-            finish()
-            sys.exit()
-
-    # Time to convert!
-    convert(fullpath, converted_path)
-   
-    printHeader()
-    print " converted"
-    finish()
-    sys.exit()
- 
-    try:
-        encoded = urllib.quote(open(converted_path, "rb").read().encode("base64"))
-        printHeader()
-        sys.stdout.write(encoded)
-        sys.stdout.flush()
+            if (write_log):
+                f.write("Returned newly converted ogg!\n")
+                f.close()
+        else:
+            if (write_log):
+                f.write("Newly converted file does not exist: " + converted_path + "\n")
+                f.close()
+                sys.exit()
     except Exception as e:
-        print(e)
-        printHeader()
-        print "Something broke in returning a newly converted ogg"
-        print better_input
-        print converted_path
-        finish()
+        if (write_log):
+            f.write("Error returning newly converted ogg!\n")
+            f.write(e)
+            f.close()
+    sys.exit()
 
